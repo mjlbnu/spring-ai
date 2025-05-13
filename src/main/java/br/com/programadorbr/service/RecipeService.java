4bf79e14d82a6a200548d112d34289497c3d4361
mjlbnu <mjlbnu@gmail.com>
2025-05-12 22:55:49 -0300

    feat: recipe service

--- src/main/java/br/com/programadorbr/service/RecipeService.java
+++ src/main/java/br/com/programadorbr/service/RecipeService.java
@@ -1,4 +1,36 @@
 package br.com.programadorbr.service;
 
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public class RecipeService {
+
+    private final ChatModel chatModel;
+
+    public RecipeService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String createRecipe(String ingredients, String cuisine, String dietaryRestrictions) {
+        var template = """
+                Create a recipe with the following ingredients: {ingredients}.
+                The recipe should be in {cuisine} cuisine.
+                Consider the following dietary restrictions: {dietaryRestrictions}.
+                """;
+        PromptTemplate promptTemplate = new PromptTemplate(template);
+        Map<String, Object> params = Map.of(
+                "ingredients", ingredients,
+                "cuisine", cuisine,
+                "dietaryRestrictions", dietaryRestrictions
+        );
+
+        Prompt prompt = promptTemplate.create(params);
+
+        return chatModel.call(prompt).getResult().getOutput().getText();
+    }
 }
